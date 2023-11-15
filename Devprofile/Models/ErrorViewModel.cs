b6a006c588f504cf439b497ad8a29e95dcdc0ab0
MhizzyOfficial <adetayosteve@gmail.com>
2023-11-15 15:04:46 +0100

    Initial feature

---
+++ Devprofile/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Devprofile.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
